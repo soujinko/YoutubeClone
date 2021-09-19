cf5393122766c8d5e08adcc90d47dc585923c11a
soujinko <soujinko@naver.com>
2021-09-20 01:34:03 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
+import styles from './app.module.css';
 
-const App = () => {
+const App = ({youtube}) => {
   const [videos, setVideos] = useState([]);
-  useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD0iZQQo7NXDybTSsJ8lFePg9Wcbm9nO7Y&=AIzaSyD0iZQQo7NXDybTSsJ8lFePg9Wcbm9nO7Y", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+  const search = query => {
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos))
+  }
+
+  useEffect(()=>{  
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos))
+ 
   }, [])
   return (
-  <div>
+  <div className={styles.app}>
+  <SearchHeader onSearch={search}/>
     <VideoList videos={videos} />
-  </div>)
+  </div>
+  )
   
 };
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import App from './app';
+import Youtube from './components/service/youtube';
+
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById('root')
 );

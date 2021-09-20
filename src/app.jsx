2b513ff8eee44787f15af95056947b47727f94b3
soujinko <soujinko@naver.com>
2021-09-20 14:35:41 +0900

    detail page added

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,18 @@ import React, { useEffect, useState } from 'react';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 import styles from './app.module.css';
+import VideoDetail from './components/video_detail/video_detail';
 
 const App = ({youtube}) => {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setselectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setselectedVideo(video)
+  }
+
   const search = query => {
+    setselectedVideo(null)
     youtube
     .search(query)
     .then(videos => setVideos(videos))
@@ -19,8 +27,16 @@ const App = ({youtube}) => {
   }, [])
   return (
   <div className={styles.app}>
-  <SearchHeader onSearch={search}/>
-    <VideoList videos={videos} />
+  <SearchHeader onSearch={search}/> 
+  <section className={styles.content}>
+    { selectedVideo && <div className={styles.detail}>
+      <VideoDetail video={selectedVideo} />
+    </div>}
+    <div className={styles.list}>
+      <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo? 'list':'grid'}/>
+    </div>
+    
+  </section>
   </div>
   )
   
